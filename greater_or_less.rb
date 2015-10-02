afdb5ee5be651c1d7569388858e5f6f13fe5891a
Leonardo Ávila <leuavila@gmail.com>
2015-10-01 23:47:01 -0300

    Encapsulated methods

--- greater_or_less.rb
+++ greater_or_less.rb
@@ -1,38 +1,46 @@
-puts "Seja bem vindo ao jogo da advinhação."
+def welcome_player
+	puts "Seja bem vindo ao jogo da advinhação."
 
-puts "Qual é o seu nome?"
+	puts "Qual é o seu nome?"
 
-name = gets
+	name = gets
 
-puts "\n\n\n"
+	puts "\n\n\n"
 
-puts "Começaremos um jogo para você," + name + "."
+	puts "Começaremos um jogo para você," + name + "."
 
-puts "\n\n\n\n"
+	puts "\n\n\n\n"
+end
 
-puts "Escolhendo um número entre 0 e 200..."
+def choose_number
+	puts "Escolhendo um número entre 0 e 200..."
 
-secret_number = 175
+	secret_number = 175
 
-puts "Escolhido! Que tal advinhar o número?"
+	puts "Escolhido! Que tal advinhar o número?"
 
-chances = 5
-
-for attempt in 1..chances
-	puts "Tentativa " + attempt.to_s + " de " + chances.to_s "."
+	secret_number
+end
 	
+def ask_a_number(attempt, chances)
+	puts "Tentativa " + attempt.to_s + " de " + chances.to_s + "."
+		
 	puts "Entre com um número:"
-	
+		
 	answer = gets
-	
+		
 	puts "Você chutou:" + answer + "."
+	
+	answer
+end	
 
+def verification(secret_number, answer)
 	right = secret_number == answer.to_i
-
+		
 	if right
 		puts "Acertou!"
 	
-		break
+		return true
 	else
 		greater = secret_number > answer.to_i
 		
@@ -41,5 +49,19 @@ for attempt in 1..chances
 		else
 			puts "O número secreto é menor."
 		end	
+		
+		false
 	end
+end
+
+welcome_player
+
+secret_number = choose_number
+
+chances = 5
+
+for attempt in 1..chances
+	answer = ask_a_number(attempt, chances)
+	
+	break if verification(secret_number, answer)
 end
