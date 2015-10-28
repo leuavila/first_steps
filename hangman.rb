b17d68d0d30f868e1e4959982004263b3af091bf
Leonardo Ávila <leuavila@gmail.com>
2015-10-27 22:24:56 -0200

    Hangman

---
+++ hangman.rb
@@ -0,0 +1,98 @@
+def break_line
+  puts "\n"
+end
+
+def welcome_player
+  puts 'Seja bem vindo ao jogo da forca.'
+
+  print 'Digite seu nome: '
+
+  name = gets.strip
+
+  break_line
+
+  puts "Começaremos um jogo para você, #{name}."
+
+  break_line
+
+  name
+end
+
+def choose_word
+  puts 'Escolhendo a palavra...'
+
+  secret_word = 'programador'
+
+  puts "A palavra secreta foi escolhida e tem #{secret_word.size} letras. Boa sorte!"
+
+  break_line
+
+  secret_word
+end
+
+def ask (tries, misses)
+  puts " Você tentou as letras #{tries} e tem #{points} pontos até agora"
+
+  break_line
+
+  print "Entre com uma letra ou palavra: "
+
+  try = gets.strip
+
+  puts "Você chutou #{try}."
+
+  break_line
+end
+
+# def verification (secret_word, ask)
+#   single_word = try.size == 1
+
+def try_again?
+  print 'Você quer jogar novamente? [S/n] '
+
+  input = gets.strip
+
+  input.upcase == 'S' || input == ''
+end
+
+def play (welcome_player)
+  secret_word = choose_word
+
+  misses = 0
+  tries = []
+  points = 0
+
+  while misses < 5
+    try = ask tries, misses
+    tries << try
+  end
+
+  single_word = try.size == 1
+
+  if single_word
+
+    else 
+      bingo = try == secret_word
+
+      if bingo
+    
+        puts 'Acertou mizeravi!'
+    
+        points += 100
+    
+        break
+      else
+        puts "You tried..."
+
+        points -= 30
+
+        misses += 1
+      end
+  end
+  puts "Você fez #{points} pontos !"
+
+loop do
+  play_with name, level
+
+  break if not try_again?
+  
